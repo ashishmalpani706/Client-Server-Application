abfa1ab93d347bbdfdb5012649924d3761eb3420
Ashish Malpani <ashishmalpani706@gmail.com>
2018-09-28 04:44:45 -0400

    Implemented file transfer ver 1

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,9 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -15,6 +18,10 @@ public class Client {
     private void start() throws IOException {
         String data;
         String input, request;
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fos;
+        BufferedOutputStream bos;
         PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
@@ -31,7 +38,27 @@ public class Client {
         System.out.println("Sending "+request);
         out.println(request);
         out.flush();
-        System.out.println(br.readLine());
+        data = br.readLine();
+        System.out.println(data);
+        if (!("Result: HTTP/1.0 404 Not Found".equals(data))) {
+            byte [] mybytearray  = new byte [100000];
+            InputStream is = socket.getInputStream();
+            fos = new FileOutputStream(input);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = is.read(mybytearray,0,mybytearray.length);
+            current = bytesRead;
+
+            do {
+                bytesRead =
+                        is.read(mybytearray, current, (mybytearray.length-current));
+                if(bytesRead >= 0) current += bytesRead;
+            } while(bytesRead > -1);
+
+            bos.write(mybytearray, 0 , current);
+            bos.flush();
+            System.out.println("File " + input
+                    + " downloaded (" + current + " bytes read)");
+        }
 
 //        while ((data = br.readLine()) != null) {
 //            System.out.println(data);
--- src/server/Server.java
+++ src/server/Server.java
@@ -10,7 +10,6 @@ public class Server {
     private String http200 = "HTTP/1.0 200 OK";
     private String http400 = "HTTP/1.0 400 Bad Request";
     private String http404 = "HTTP/1.0 404 Not Found";
-
     public Server(int port) throws Exception {
         if (port > 0) {
             myServer = new ServerSocket(port, 1, InetAddress.getLocalHost());
@@ -19,31 +18,48 @@ public class Server {
             myServer = new ServerSocket(0, 1, InetAddress.getLocalHost());
         }
     }
-
     private void listen() throws Exception {
         String data;
         Socket client;
         String clientAddress;
         BufferedReader br;
         PrintWriter out;
-
+        FileInputStream fis;
+        BufferedInputStream bis;
+        OutputStream os;
         while (true) {
             System.out.println("Waiting for connection");
             client = this.myServer.accept();
             clientAddress = client.getInetAddress().getHostAddress();
             System.out.println("Connection from: " + clientAddress);
-
             br = new BufferedReader(new InputStreamReader(client.getInputStream()));
             out = new PrintWriter(client.getOutputStream(), true);
-
             try {
                 while ((data = br.readLine()) != null) {
                     System.out.println("Message from " + clientAddress + ": " + data);
-                    System.out.println(parse(data));
-
-                    out.println("Result: "+parse(data));
-                    out.flush();
-
+                    System.out.println("Response: " +parse(data));
+                    if (parse(data).equals(http400)) {
+                        out.println("Result: " + parse(data));
+                        out.flush();
+                    }
+                    else {
+                        File myFile = new File(parse(data));
+                        if (!myFile.exists()) {
+                            System.out.println("Response: " +http404);
+                            out.println("Result: " + http404);
+                            out.flush();
+                        }
+                        else {
+                            byte[] mybytearray  = new byte[(int)myFile.length()];
+                            fis = new FileInputStream(myFile);
+                            bis = new BufferedInputStream(fis);
+                            bis.read(mybytearray,0,mybytearray.length);
+                            os = client.getOutputStream();
+                            System.out.println("Sending " + parse(data) + "(" + mybytearray.length + " bytes)");
+                            os.write(mybytearray,0,mybytearray.length);
+                            os.flush();
+                        }
+                    }
                 }
             } catch (SocketException e) {
                 System.out.println("Connection Terminated");
@@ -53,7 +69,6 @@ public class Server {
             }
         }
     }
-
     public String parse(String request) {
         String fileName = "";
         if (request.matches("^(GET).+HTTP/1.0")) {
@@ -70,18 +85,9 @@ public class Server {
         }
         return fileName;
     }
-
-    public String getSocketAddress() {
-        return this.myServer.getInetAddress().getHostAddress();
-    }
-
-    public int getPort() {
-        return this.myServer.getLocalPort();
-    }
-
     public static void main(String args[]) throws Exception {
         Server s1 = new Server(Integer.valueOf(args[0]));
-        System.out.println("Server is running at: "+s1.getSocketAddress()+" at port: "+s1.getPort());
+        System.out.println("Server is running at: "+s1.myServer.getInetAddress().getHostAddress()+" at port: "+s1.myServer.getLocalPort());
         s1.listen();
     }
 }
